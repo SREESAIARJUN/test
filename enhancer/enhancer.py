21efde3911736204405ea4a9a02e07ce47e372af
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-04-27 13:27:12 +0530

    Update enhancer.py

--- enhancer/enhancer.py
+++ enhancer/enhancer.py
@@ -1,6 +1,8 @@
 class Enhancer:
     def __init__(self, method, background_enhancement, upscale):
-        pass
+        self.method = method
+        self.background_enhancement = background_enhancement
+        self.upscale = upscale
 
     def enhance(self, image):
-        pass
+        return image
