147b342f1feaf8eaf6e8aae4b9487471fa5a0a68
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2025-04-27 13:28:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ import argparse
 
 def main(method, image_path, output_path, background_enhancement, upscale):
     # Create enhancer
-    enhancer = Enhancer(method=method, background_enhancement=background_enhancement, upscale=upscale)
+    enhancer = Enhancer(method=method, background_enhancement=background_enhancement, upscale=upscale)    
+    image = Image.open(image_path)
+    image = np.array(image)
 
-    image = np.array(Image.open(image_path))
-    restored_image = enhancer.enhance(image)
+    restored_image = enhancer.enhance(image)    
 
     final_image = Image.fromarray(restored_image)
     final_image.save(output_path)
@@ -25,7 +26,7 @@ if __name__ == "__main__":
     # Add output image argument
     parser.add_argument("--output_path", type=str, required=True, help="Specify the output path.")
     # Add background enhancement argument
-    parser.add_argument("--background_enhancement", action="store_true", help="Specify the background enhancement option.", default=True)
+    parser.add_argument("--background_enhancement", action="store_true", help="Specify the background enhancement option.", default=False)
     # Add enhancement upscale argument
     parser.add_argument("--upscale", type=int, help="Specify the enhancement scale (2, 4).")
     # Parse the arguments
